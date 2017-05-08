31e44b70ba48547e7cae23e2249c699f73f151da
benBen <benmbenjamin@gmail.com>
2017-05-08 13:58:18 +0200

    working on home page

--- home.php
+++ home.php
@@ -25,6 +25,21 @@
 
     ?>
     </div>
+
+    <div class="table-of-contents absolute text-center">
+      <span class="title block">
+        <h2>Surfers Lodge</h2>
+        <h3>Peniche</h3>
+      </span>
+      <?php
+        wp_nav_menu( array('menu' => 'Home Menu') );
+
+        $custom_logo_id = get_theme_mod( 'custom_logo' );
+        $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+      ?>
+      <img class="logo block" src="<?php echo $logo[0] ?>" />
+    </div>
+
   </div>
 
 <?php get_footer() ?>
--- js/script.js
+++ js/script.js
@@ -1,33 +1,3 @@
-// console.log(TweenMax)
-
-// ;(function() {
-
-//   var root = null;
-//   // var useHash = false;
-//   // var hash = '#!';
-//   var router = new Navigo(root);
-
-//   var anchors = Array.from(document.getElementsByTagName("a"))
-//   console.log(anchors)
-//   anchors.forEach(function(an, i) {
-//     var href = an.getAttribute("href")
-//     router.on(href, function() {
-//       console.log("routing", href)
-//     }).resolve()
-
-//     an.addEventListener("click", function(e) {
-//       e.preventDefault()
-//       router.navigate(href)
-//     })
-//   })
-
-//   router
-//     .on('/peniche/lodge', function() {
-//       console.log("fudge")
-//     })
-//     .resolve();
-
-// })();
 window.$ = window.jQuery
 $(document).ready(function() {
   var $home = $("#Home")
@@ -45,8 +15,6 @@ $(document).ready(function() {
     window.setInterval(function() {
       update_slide()
     }, 3000)
-    // var home_bg_urls = home_bg.getAttribute("data-urls").split(",")
-    // home_bg.style.backgroundImage = "url("+home_bg_urls[0]+")"
   }
 
 })
